b244c8fdccbea81c8baef763bbbb22f0e7fdfeec
abaisero <andrea.baisero@gmail.com>
2023-02-22 15:54:58 -0500

    fix return and find bugs

--- bin_experiments/progress_tasks.sh
+++ bin_experiments/progress_tasks.sh
@@ -3,8 +3,8 @@
 experiment_id=$1
 shift
 
-ntasks=$(find task_files/ -name "$experiment_id*.BEGUN" -maxdepth 1 -type f | wc -l)
-ndone=$(find task_files/ -name "$experiment_id*.DONE" -maxdepth 1 -type f | wc -l)
+ntasks=$(find task_files/ -name "$experiment_id*.BEGUN" -type f | wc -l)
+ndone=$(find task_files/ -name "$experiment_id*.DONE" -type f | wc -l)
 
 printf "%4d / %4d tasks done\n" $ndone $ntasks
 
