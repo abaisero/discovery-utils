5d744b1640cc8cdc57f086aa33cf96371a3ee582
abaisero <andrea.baisero@gmail.com>
2023-09-05 11:19:05 -0400

    fix wandb_log.sh script when there are no current jobs

--- bin_experiments/wandb_log.sh
+++ bin_experiments/wandb_log.sh
@@ -8,8 +8,13 @@ sbatch_args=(
   --time 24:00:00
   --job-name=wandb-log
   --output=wandb-log.out
-  --dependency=singleton,afterany:$job_ids 
 )
+
+if [[ $job_ids =~ [0-9] ]]; then
+  sbatch_args+=(--dependency=singleton,afterany:$job_ids)
+else
+  sbatch_args+=(--dependency=singleton)
+fi
 
 # Submit the new job with a dependency on all running and pending jobs
 sbatch ${sbatch_args[@]} --wrap 'wandb sync --sync-all --append'
