948b3756811e7ba8611da673fda600cd796e7d2a
abaisero <andrea.baisero@gmail.com>
2024-05-01 10:29:39 -0400

    fix bash linting

--- bin_experiments/wandb-log.sh
+++ bin_experiments/wandb-log.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Find the IDs of all running and pending jobs owned by the current user
-job_ids=$(squeue -u $USER -t RUNNING,PENDING --noheader --format="%i" | paste -sd: -)
+job_ids=$(squeue -u "$USER" -t RUNNING,PENDING --noheader --format="%i" | paste -sd: -)
 
 sbatch_args=(
   --partition short
@@ -11,10 +11,10 @@ sbatch_args=(
 )
 
 if [[ $job_ids =~ [0-9] ]]; then
-  sbatch_args+=(--dependency=singleton,afterany:$job_ids)
+  sbatch_args+=("--dependency=singleton,afterany:$job_ids")
 else
   sbatch_args+=(--dependency=singleton)
 fi
 
 # Submit the new job with a dependency on all running and pending jobs
-sbatch ${sbatch_args[@]} --wrap 'wandb sync --sync-all --append'
+sbatch "${sbatch_args[@]}" --wrap 'wandb sync --sync-all --append'
