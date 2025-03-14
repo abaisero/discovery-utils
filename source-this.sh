47e6dbf0563b0d64d801145b5a0cdc402c1bcc71
abaisero <andrea.baisero@gmail.com>
2025-03-14 14:17:22 -0400

    minor changes

--- source-this.sh
+++ source-this.sh
@@ -3,8 +3,8 @@
 filepath=$(readlink -f "${BASH_SOURCE:-$0}")
 dirpath=$(dirname "$filepath")
 
-export PATH="$dirpath/bin_slurm:$PATH"
-export PATH="$dirpath/bin_experiments:$PATH"
+export PATH="$dirpath/bin-slurm:$PATH"
+export PATH="$dirpath/bin-experiments:$PATH"
 
 export TASK_BEGUN="TASK.BEGUN"
 export TASK_DONE="TASK.DONE"
