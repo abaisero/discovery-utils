5d0f5f11151c90f5a363e7a9f82b359e8122435c
abaisero <baisero.a@husky.neu.edu>
2025-05-27 04:40:07 -0400

    fix bug with du-experiment-unfinished.sh

--- bin-experiments/du-experiment-unfinished.sh
+++ bin-experiments/du-experiment-unfinished.sh
@@ -3,4 +3,4 @@
 experiment_path=$1
 shift
 
-find "$experiment_path" -name "$TASK_DONE" -execdir [ ! -e "$TASK_DONE" ] \; -printf %h\\n
+find "$experiment_path" -name "$TASK_BEGUN" -execdir [ ! -e "$TASK_DONE" ] \; -printf %h\\n
